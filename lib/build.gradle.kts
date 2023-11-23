b2962ecc680fb8229c702d1b75ab67fdee3e1a86
jinlee0 <wls4370@gmail.com>
2023-11-23 16:14:10 +0900

    Publishing Maven Central

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -3,8 +3,6 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.10"
     `java-library`
-    `maven-publish`
-    signing
 }
 
 group = "io.github.jinlee0"
@@ -25,79 +23,8 @@ java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
-    withJavadocJar()
-    withSourcesJar()
 }
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
-}
-
-tasks.javadoc {
-    if (JavaVersion.current().isJava9Compatible) {
-        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            groupId = group.toString()
-            artifactId = "rusult"
-            version = version
-            from(components["java"])
-
-            versionMapping {
-                usage("java-api") {
-                    fromResolutionOf("runtimeClasspath")
-                }
-                usage("java-runtime") {
-                    fromResolutionResult()
-                }
-            }
-
-            pom {
-                name = "rusult"
-                packaging = "jar"
-                description = "Rust-like result written in Kotlin"
-                licenses {
-                    license {
-                        name = "MIT License"
-                        url = "http://www.opensource.org/licenses/mit-license.php"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "jinlee0"
-                        name = "Jin-Yong Lee"
-                        email = "wls4370@gmail.com"
-                    }
-                }
-                scm {
-                    connection = "scm:git:git://github.com/jinlee0/rusult.git"
-                }
-            }
-        }
-    }
-    repositories {
-        maven {
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = "username"
-                password = "password"
-            }
-        }
-    }
-
-}
-
-
-signing {
-    sign(publishing.publications["mavenJava"])
-}
-
-tasks.javadoc {
-    if (JavaVersion.current().isJava9Compatible) {
-        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
-    }
 }
